d595405c6e9114321c42b2dea8094000201d4ed3
Maria Eduarda Tavares <madutavares.ofc@gmail.com>
2025-07-31 17:45:49 -0300

    Python-exercises

---
+++ beginner/012.py
@@ -0,0 +1,3 @@
+p = float(input('Digite o preço atual do produto: '))
+np = p - (p*(5/100))
+print('O valor {} com desconto de 5%, fica: {}' .format(p, np))
---
+++ beginner/013.py
@@ -0,0 +1,3 @@
+s = float(input('Digite o seu salário atual: '))
+ns = s + (s*(15/100))
+print('O salário de valor {} corrigido atualmente vale {}!' .format(s, ns))
