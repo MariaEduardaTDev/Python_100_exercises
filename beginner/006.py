016721d3441110d7be73eec95cbda12968a34cdd
Maria Eduarda Tavares <madutavares.ofc@gmail.com>
2025-07-31 17:49:49 -0300

    Update 006.py

--- beginner/006.py
+++ beginner/006.py
@@ -3,4 +3,4 @@ N2 = float(input('Digite um número decimal: '))
 
 print('O dobro de {} é {} \n O dobro de {} é {}' .format(N1, N1*2, N2, N2*2))
 print('O triplo de {} é {} \n O triplo {} é {}' .format(N1, N1*3, N2, N2*3))
-print('A raiz quadrada de {} é {} \n A raiz quadrada de {} é {}' .format(N1, N1**2, N2, N2**2))
+print('A raiz quadrada de {} é {} \n A raiz quadrada de {} é {}' .format(N1, N1**(1/2), N2, N2**(1/2)))
