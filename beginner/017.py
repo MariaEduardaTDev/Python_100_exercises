7e91bba3ea1a6c5535cd343dbb52802237592d65
Maria Eduarda Tavares <madutavares.ofc@gmail.com>
2025-08-08 09:35:49 -0300

    Create 017.py

---
+++ beginner/017.py
@@ -0,0 +1,19 @@
+import math
+print('\nEsse programa irá calcular o Seno, Cosseno e Tangente de um ângulo qualquer.\n')
+
+ang = int(input('Digite o valor do ângulo em graus: '))
+angR = math.radians(ang)
+
+seno = math.sin(angR)
+cosseno = math.cos(angR)
+tangente = math.tan(angR)
+
+
+print('seno = {:.2f}' .format(seno))
+print('cosseno = {:.2f}' .format(cosseno))
+
+if cosseno < 1e-10:
+    print('\ntangente = indefinida.')
+
+else:
+    print('tangente = {:.2f}' .format(tangente))
