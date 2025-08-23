44daed4c5591168d1e84dd7901c8e4594defde16
MariaEduardaTDev <madutavares.ofc@gmail.com>
2025-08-23 00:16:11 -0300

    Create test06.py

---
+++ beginner/test06.py
@@ -0,0 +1,13 @@
+import random
+import emoji
+
+print(emoji.emojize("Hello World :globe_showing_Americas:"))
+
+num = random.random()
+print(num)
+
+num2 = random.randint(1,99)
+print(num2)
+
+num3 = random.choice("ANDKJSKB")
+print(num3)
